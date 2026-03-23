435e93fb9bcff798f3ea79df0419e092e82339cb
Ajaydeep Singh <asingh183@rrc.ca>
2026-03-23 15:26:34 -0500

    Created interface for resource

---
+++ src/api/v1/types/resource.ts
@@ -0,0 +1,8 @@
+export interface Resource {
+    id: number;
+    title: string;
+    type: "article" | "video" | "tutorial" | "documentation";
+    url: string;
+    description: string;
+    createdAt: string;
+}
